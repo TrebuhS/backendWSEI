669bdc21b176494f091b0bfe2dc60de3a525e7e0
Hubert Sośnicki <37559824+TrebuhS@users.noreply.github.com>
2022-03-12 14:17:23 +0100

    Finish 4 excercise

--- src/com/company/Main4.java
+++ src/com/company/Main4.java
@@ -1,28 +1,25 @@
 package com.company;
 
 public class Main4 {
+    static int[] CRC_TABLE = new int[256];
 
     public static void main(String[] args) {
-        int array[] = { 4, 5, 7, 11, 12, 15, 15, 21, 40, 45 };
-        System.out.println(searchIndex(array, 11));
+        for (int i = 0; i < 256; ++i) {
+            int code = i;
+            for (int j = 0; j < 8; ++j) {
+                code = ((code & 0x01) > 0 ? 0xEDB88320 ^ (code >>> 1) : (code >>> 1));
+            }
+            CRC_TABLE[i] = code;
+        }
+        System.out.println(crc32("This is example text..."));
     }
 
-    public static int searchIndex(int[] array, int value) {
-        int index = 0;
-        int limit = array.length - 1;
-        while (index <= limit) {
-            int point = (int)Math.ceil((index + limit) / 2);
-            int entry = array[point];
-            if (value > entry) {
-                index = point + 1;
-                continue;
-            }
-            if (value < entry) {
-                limit = point - 1;
-                continue;
-            }
-            return point;
+    private static int crc32(String text) {
+        int crc = -1;
+        for (int i = 0; i < text.length(); ++i) {
+            int code = text.codePointAt(i);
+            crc = CRC_TABLE[(code ^ crc) & 0xFF] ^ (crc >>> 8);
         }
-        return -1;
+        return (-1 ^ crc) >>> 0;
     }
 }
