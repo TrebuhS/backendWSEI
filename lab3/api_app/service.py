818639e9ed5fcc8a200ad1c6f7f4ba6b42a40f2c
Hubert <trebuh@MacBook-Pro-Hubert-2.local>
2022-04-30 13:31:29 +0200

    Add service layer

---
+++ lab3/api_app/service.py
@@ -0,0 +1,38 @@
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
+
+from api_app.models import UserModel
+from api_app.serializers import UserModelSerializer
+
+
+class UsersService:
+
+    def create_user(self, data):
+        serializer = UserModelSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data
+
+    def get_users(self, page_number=0, page_size=20):
+        users = list(UserModel.objects.all())
+        paginator = Paginator(users, page_size)
+        return paginator.get_page(page_number)
+
+    def get_user(self, id):
+        user = get_object_or_404(UserModel, id=id)
+        serializer = UserModelSerializer(user)
+        return serializer.data
+
+    def delete_user(self, id):
+        user = get_object_or_404(UserModel, id=id)
+        user.delete()
+
+    def update_user(self, id, data):
+        user = get_object_or_404(UserModel, id=id)
+        serializer = UserModelSerializer(user, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data
+        else:
+            return ValidationError("Wrong object")
--- lab3/api_app/views.py
+++ lab3/api_app/views.py
@@ -1,51 +1,49 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from .models import UserModel
 from .serializers import UserModelSerializer
-from rest_framework.decorators import action
+from .service import UsersService
 
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserModelSerializer
-    def get_queryset(self):
-        return
+    service = UsersService()
+
     @action(methods=['post'], detail=False, url_path='create')
     def create_user(self, request):
-        serializer = UserModelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        user = self.service.create_user(request.data)
+        return Response(user, status=status.HTTP_200_OK)
 
     @action(methods=['get'], detail=False)
-    def get_user(self, request, page_number=0, page_size=20):
+    def get_user(self, request, page_number=None, page_size=None):
         page_number = request.Get.get('page_number')
         page_size = request.Get.get('page_size')
-        queryset = UserModel.objects.all()
-        paginator = Paginator(queryset, page_size)
-        return Response({"status": "success", "data": paginator.page(page_number)}, status=status.HTTP_200_OK)
+        users = self.service.get_users(page_number, page_size)
+        return Response(users, status=status.HTTP_200_OK)
 
     @action(methods=['get'], detail=False, url_path=r'(?P<id>\w+)')
     def get_user(self, request, id):
-        user = get_object_or_404(UserModel, id=id)
-        serializer = UserModelSerializer(user)
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        user = self.service.get_user(id)
+        return Response(user, status=status.HTTP_200_OK)
 
     @action(methods=['delete'], detail=False, url_path=r'(?P<id>\w+)/remove')
     def delete_user(self, request, id):
-        user = get_object_or_404(UserModel, id=id)
-        user.delete()
+        self.service.delete_user(id)
         return Response({"result": True})
 
     @action(methods=['patch'], detail=False, url_path=r'(?P<id>\w+)/update')
     def delete_user(self, request, id):
-        user = get_object_or_404(UserModel, id=id)
-        serializer = UserModelSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
+        try:
+            user = self.service.update_user(id, request.data)
+            return Response(user, status=status.HTTP_200_OK)
+        except ValidationError:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def get_queryset(self):
+        return
