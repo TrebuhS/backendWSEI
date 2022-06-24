93978447f9f79830c45c9ce7c77b53186975952d
Hubert <trebuh@MacBook-Pro-Hubert-2.local>
2022-06-24 20:44:03 +0200

    Add user model and AppRequest

---
+++ project/management/app/models/users/user_details.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+    friends: List[int]
